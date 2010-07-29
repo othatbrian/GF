ffa9de12a6c70f8f76e6da0d59cd6b628662939f
Brian Stevens <brian@stevens.com>
2010-07-29 08:38:02 -0500

    Working connection(s) to GFProject(s)

--- lib/gf_project.rb
+++ lib/gf_project.rb
@@ -1,13 +1,88 @@
 $: << File.expand_path('..', __FILE__)
 
+require 'open3'
 require 'gf_catalog'
 
 class GFProject
-  attr_accessor :name
-  attr_accessor :account
+  attr_reader :name
+  attr_reader :account
   
-  def initialize(name, account)
+  def initialize(name, account, password = nil)
     @name = name
     @account = account
+    
+    catch :connected do
+      errors = ''
+      if password
+        begin
+          throw(:connected) if connect_with_supplied_password(password)
+        rescue
+          errors = $!.to_s
+        end
+      else
+        errors = "project password is not set" unless password
+      end
+      begin
+        throw(:connected) if connect_with_default_password
+      rescue
+        errors += " and " + $!.to_s
+      end
+      if ENV[:GFDBA_PASSWORD]
+        begin
+          throw(:connected) if connect_with_gfdba_password
+        rescue
+          errors += " and " + $!.to_s
+        end
+      else
+        errors += " and GFDBA_PASSWORD is not set"
+      end
+      raise(RuntimeError, errors)
+    end
+    self
+  end
+  
+  def bulk_server_command
+    query do |db|
+      db[:paramter].
+      select(:value_string).
+      filter(:code => 'Bulk_Server_Command').
+      first[:value_string]
+    end
+  end
+  
+  private
+  def connect(password)
+    Sequel.oracle(:database => ENV[:TWO_TASK], :user => @account, :password => password, :test => true)
+  end
+  
+  def connect_with_default_password
+    begin
+      connect @name
+    rescue Sequel::DatabaseConnectionError
+      raise RuntimeError, 'project default password is not valid'
+    end
+  end
+  
+  def connect_with_gfdba_password
+    begin
+      Open3.popen3("proj_get_password #{name} bogus") do |stdin, stdout, stderr|
+        errors = stderr.read
+        if errors =~ /^Invalid password/m
+          raise RuntimeError, 'GFDBA_PASSWORD is not valid'
+        end
+        stdout.read =~ /^Password =(.*)$/m
+        connect $1.chomp
+      end
+    rescue Errno::ENOENT
+      raise RuntimeError, 'proj_get_password not found'
+    end
+  end
+  
+  def connect_with_supplied_password(password)
+    begin
+      connect password
+    rescue Sequel::DatabaseConnectionError
+      raise RuntimeError, "project password \"#{password}\" is not valid"
+    end
   end
 end
--- test/gf_project_test.rb
+++ test/gf_project_test.rb
@@ -5,22 +5,75 @@ require 'gf_project'
 
 class GFProjectTest < Test::Unit::TestCase
   def test_name_is_readable
-    project = GFProject.new :test, :TEST
-    assert_equal :test, project.name
-  end
-  
-  def test_name_is_writable
-    project = GFProject.new :test, :TEST
-    assert_nothing_raised { project.name = :test }
+    project = GFProject.new :test1, :TEST1, :test1
+    assert_equal :test1, project.name
   end
   
   def test_account_is_readable
-    project = GFProject.new :test, :TEST
+    project = GFProject.new :test1, :TEST1, :test1
     assert_nothing_raised { project.account }
   end
+
+  def test_password_invalid_default_invalid_GFDBA_not_set_message
+    unset_gfdba_password do
+      assert_block do
+        begin
+          GFProject.new :test2, :TEST2, :foo
+        rescue
+          $!.to_s == 'project password "foo" is not valid and project default password is not valid and GFDBA_PASSWORD is not set'
+        end
+      end
+    end
+  end
   
-  def test_account_is_writable
-    project = GFProject.new :test, :TEST
-    assert_nothing_raised { project.account = :test }
+  def test_password_not_set_default_invalid_GFDBA_not_set_message
+    unset_gfdba_password do
+      assert_block do
+        begin
+          GFProject.new :test2, :TEST2
+        rescue
+          $!.to_s == 'project password is not set and project default password is not valid and GFDBA_PASSWORD is not set'
+        end
+      end
+    end
+  end
+
+  def test_password_invalid_default_invalid_GFDBA_invalid_message
+    unset_gfdba_password do
+      ENV[:GFDBA_PASSWORD] = :bogus
+      assert_block do
+        begin
+          GFProject.new :test2, :TEST2, :foo
+        rescue
+          $!.to_s == 'project password "foo" is not valid and project default password is not valid and GFDBA_PASSWORD is not valid'
+        end
+      end
+    end
+  end
+ 
+  def test_password_not_set_default_invalid_GFDBA_invalid_message
+    unset_gfdba_password do
+      ENV[:GFDBA_PASSWORD] = :bogus
+      assert_block do
+        begin
+          GFProject.new :test2, :TEST2
+        rescue
+          $!.to_s == 'project password is not set and project default password is not valid and GFDBA_PASSWORD is not valid'
+        end
+      end
+    end
+  end
+
+#  def test_bulk_server_command
+#    project = GFProject.new :test1, :TEST1
+#    assert_equal 'foo', project.bulk_server_command
+#  end
+
+  private
+  def unset_gfdba_password
+    gfdba = ENV[:GFDBA_PASSWORD] if ENV[:GFDBA_PASSWORD]
+    ENV[:GFDBA_PASSWORD] = nil
+    yield
+    ENV[:GFDBA_PASSWORD] = gfdba if gfdba
   end
 end
